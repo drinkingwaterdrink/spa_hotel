3b9a6639b9b41fa8b1948cb9283707e24ee93b32
Grant <55523403+Grant-Hwang@users.noreply.github.com>
2022-11-24 20:13:29 +0900

    client change 1

--- src/Client.java
+++ src/Client.java
@@ -1,28 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Client {   //고객
+    Scanner sc = new Scanner(System.in);
 
-    String name;    //예약자 이름
-    String number;  //예약자 번호
-    int money;  //소지금
+    String phonePattern = "^\\d{3}-\\d{4}-\\d{4}$";
 
-    String id;  //여기다가 저장하는게 맞는지?
-    //저장하는게맞다면, 다중예약이된다면 리스트형태로 만들어서 보관?
+    List<ClientList> arr = new ArrayList<>();
 
+    public Client() {}
 
+    public void addClient() {
+        System.out.println("이름을 입력해주세요.");
+        String cName = sc.nextLine();
+        System.out.println("전화번호를 입력해주세요.");
+        String cNumber = sc.nextLine();
 
-    public void 예약목록조회(예약번호){
+        while (Pattern.matches(phonePattern, cNumber) == false) {
+            System.out.println("올바른 전화번호를 입력해주세요.");
+            cNumber = sc.nextLine();
+        }
+        System.out.println("소지금액을 입력해주세요.");
+        int cMoney = sc.nextInt();
+        sc.nextLine();
 
+        this.arr.add(new ClientList(cName, cNumber, cMoney));
     }
 
-    public void 예약취소(){
 
+
+    public void getClientInfo() {
+        String cNumber = sc.nextLine();
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            if (cNumber.equals(this.arr.get(i).getNumber())) {
+                System.out.println(arr.get(i) + "\n");
+            }
+
+        }
     }
 
-    public void reserv_client(){
-        Reserv reserv = new Reserv();
-        reserv.reserv_hotel();
-
-
-    }
+//    public void reserv_client(){
+//        Reserv reserv = new Reserv();
+//        reserv.reserv_hotel();
+//
+//
+//    }
 
 
 

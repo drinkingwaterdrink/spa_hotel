98fd284daaf5cfc17b4c1790e6efc887b12334a6
Grant <55523403+Grant-Hwang@users.noreply.github.com>
2022-11-25 08:56:04 +0900

    uuid update

--- src/ClientList.java
+++ src/ClientList.java
@@ -1,14 +1,17 @@
+import java.util.UUID;
+
 public class ClientList {
     String name;    //예약자 이름
     String number;  //예약자 번호
     int money;  //소지금
-    String id;  //여기다가 저장하는게 맞는지?
+    UUID id;  //여기다가 저장하는게 맞는지?
     //저장하는게맞다면, 다중예약이된다면 리스트형태로 만들어서 보관?
 
-    public ClientList(String cName, String cNumber, int cMoney) {
+    public ClientList(String cName, String cNumber, int cMoney, UUID cId) {
         this.name = cName;
         this.number = cNumber;
         this.money = cMoney;
+        this.id = cId;
     }
 
 
@@ -20,7 +23,8 @@ public class ClientList {
     public String toString() {
         return "\n예약자 이름 : " + name +
                "\n예약자 번호 : " + number +
-               "\n소지금 : " + money
+               "\n소지금 : " + money +
+               "\n예약 아이디 : " + id
                ;
     }
 

f500570e00cffda4e18c680d8593d2d8d005e8dd
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2022-11-25 12:29:52 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,77 +5,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Admin admin = new Admin("1234");
         Hotel hotel = new Hotel(0);
+        ClientList clientList = new ClientList();
 
 
         int choice;
         boolean flag = false;
         Reserv reserveList = new Reserv();
 
+        System.out.println("=====================");
+        System.out.println("ㅐ***Hotel 911***ㅐ");
+        System.out.println("1. 관리자 화면은 숫자 '1'을, 2. 고객이시면 숫자 '2'를 눌러주세요.");
+        System.out.println("=====================");
+        Scanner indexInput = new Scanner(System.in);
+        int num = indexInput.nextInt();
 
         //관리자 화면
-        while(true) {
-            System.out.println("=====================");
-            System.out.println("ㅐ***Hotel 911***ㅐ");
-            System.out.println("1. 관리자 화면은 숫자 '1'을, 2. 고객이시면 숫자 '2'를 눌러주세요.");
-            System.out.println("=====================");
-            Scanner indexInput = new Scanner(System.in);
-            int num = indexInput.nextInt();
-
-            switch (num) {
-                case 1:
-                    if (flag == true) {
-                        flag = false;
-                        break;
-                    }
-                    System.out.println("관리자 화면입니다.");
-                    System.out.println("관리자 비밀번호를 입력해주세요");
-                    String adminPassword = sc.nextLine();
-                    if (adminPassword.equals(admin.getPassword())) {
-                        while (true) {
-                            System.out.println("\n1. 모든예약내역 확인 2.예약 취소 3.예약가능한객실 확인 4. 종료");
-                            System.out.println("숫자를 입력해주세요.");
-
-                            choice = sc.nextInt();
-                            sc.nextLine(); // 없으면 아래 sc.nextLine() 입력이 이상하게 된다.
-
-                            switch (choice) {
-                                case 1:
-                                    hotel.showAllReservList();
-                                    break;
-                                case 2:
-                                    System.out.println("취소하고자하는 예약 id를 입력해주세요");
-                                    String inputId = sc.nextLine();
-                                    hotel.cancleReserv(inputId);
-                                    break;
-                                case 3:
-                                    System.out.println("조회하고자하는 날짜를 숫자로 입력해주세요");
-                                    int Inputdate = sc.nextInt();
-                                    hotel.showReservableRoomList(Inputdate);
-                                    break;
-                                case 4:
-                                    System.out.println("관리자 모드를 종료합니다.");
-                                    flag = true;
-                                    break;
-                                default:
-                                    System.out.println("\n================================");
-                                    System.out.println("****1~4 사이 숫자를 입력해주세요****");
-                                    System.out.println("================================\n");
-                                    break;
-                            }
-                            if (flag == true) break;
-                        }
-                    } else {
-                        System.out.println("관리자 비밀번호가 틀렸습니다.");
-                    }
-                    //고객 화면
-                case 2:
-                    if (flag == true) {
-                        flag = false;
-                        break;
-                    }
-                    System.out.println("고객 화면입니다.");
+        switch (num) {
+            case 1:
+                System.out.println("\n관리자 화면입니다.");
+                System.out.println("관리자 비밀번호를 입력해주세요");
+                String adminPassword = sc.nextLine();
+                if (adminPassword.equals(admin.getPassword())) {
                     while (true) {
-                        System.out.println("\n1. 예약 하기 2. 예약 조회 3. 예약 취소 4. 예약 가능객실보기 5. 종료");
+                        System.out.println("\n1. 모든예약내역 확인 2.예약 취소 3.예약가능한객실 확인 4. 종료");
                         System.out.println("숫자를 입력해주세요.");
 
                         choice = sc.nextInt();
@@ -83,27 +35,82 @@ public class Main {
 
                         switch (choice) {
                             case 1:
+                                hotel.showAllReservList();
                                 break;
                             case 2:
+                                System.out.println("취소하고자하는 예약 id를 입력해주세요");
+                                String inputId = sc.nextLine();
+                                hotel.cancleReserv(inputId);
                                 break;
                             case 3:
-                                ;
+                                System.out.println("조회하고자하는 날짜를 숫자로 입력해주세요");
+                                int Inputdate = sc.nextInt();
+                                hotel.showReservableRoomList(Inputdate);
                                 break;
                             case 4:
-                                break;
-                            case 5:
                                 System.out.println("이용 감사합니다.");
                                 flag = true;
                                 break;
                             default:
                                 System.out.println("\n================================");
-                                System.out.println("****1~5 사이 숫자를 입력해주세요****");
+                                System.out.println("****1~4 사이 숫자를 입력해주세요****");
                                 System.out.println("================================\n");
                                 break;
                         }
                         if (flag == true) break;
                     }
-            }
+                }
+                else {
+                    System.out.println("관리자 비밀번호가 틀렸습니다.");
+                    break;
+                }
+                //고객 화면
+            case 2:
+                System.out.println("\n고객 화면입니다.");
+                while (true) {
+                    System.out.println("\n1. 예약 하기 2. 예약 조회 3. 예약 취소 4. 예약 가능객실보기 5. 종료");
+                    System.out.println("숫자를 입력해주세요.");
+
+                    choice = sc.nextInt();
+                    sc.nextLine(); // 없으면 아래 sc.nextLine() 입력이 이상하게 된다.
+
+                    switch (choice) {
+                        case 1:
+                            clientList.addClient();
+                            System.out.println("예약하고자하는 날짜를 입력해주세요 ex) 2022년11월25일 -> 20221125");
+                            int inputDate = sc.nextInt();
+                            System.out.println("예약하고자하는 객실번호를 입력해주세요 ex) 301호 -> 301");
+                            int inputRoomNum = sc.nextInt();
+                            hotel.addReserv(clientList.getClient(),inputDate,inputRoomNum);
+                            System.out.println("예약이 완료되었습니다.");
+                            break;
+                        case 2:
+                            System.out.println("조회하고자하는 예약 id를 입력해주세요");
+                            String inputId = sc.nextLine();
+                            hotel.showClientReserv(inputId);
+                            break;
+                        case 3:
+                            System.out.println("취소하고자하는 예약 id를 입력해주세요");
+                            String inputId2 = sc.nextLine();
+                            hotel.cancleReserv(inputId2);
+                            break;
+                        case 4:
+                            System.out.println("조회하고자하는 날짜를 숫자로 입력해주세요");
+                            int Inputdate = sc.nextInt();
+                            hotel.showReservableRoomList(Inputdate);
+                            break;
+                        case 5:
+                            System.out.println("이용 감사합니다.");
+                            flag = true;
+                            break;
+                        default:
+                            System.out.println("\n================================");
+                            System.out.println("****1~5 사이 숫자를 입력해주세요****");
+                            System.out.println("================================\n");
+                            break;
+                    }
+                    if (flag == true) break;
+                }
         }
     }
 }

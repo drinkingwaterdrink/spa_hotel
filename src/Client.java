27f9d228953435f0acf5140fdccd039ba7c8462f
Grant <55523403+Grant-Hwang@users.noreply.github.com>
2022-11-25 09:49:54 +0900

    Update and rename ClientList.java to Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,13 @@
 import java.util.UUID;
 
-public class ClientList {
+public class Client {
     String name;    //예약자 이름
     String number;  //예약자 번호
     int money;  //소지금
     UUID id;  //여기다가 저장하는게 맞는지?
     //저장하는게맞다면, 다중예약이된다면 리스트형태로 만들어서 보관?
 
-    public ClientList(String cName, String cNumber, int cMoney, UUID cId) {
+    public Client(String cName, String cNumber, int cMoney, UUID cId) {
         this.name = cName;
         this.number = cNumber;
         this.money = cMoney;

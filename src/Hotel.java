19494e6a14ade2398d0caa0d2e32b117d4a7c857
Grant <55523403+Grant-Hwang@users.noreply.github.com>
2022-11-28 10:03:43 +0900

    passed_date_cancel_update

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,14 +1,138 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class Hotel {    //호텔
-
-    Room room;  //객실
-    List<Room> room = new ArrayList<>();
+    RoomList roomList = new RoomList();
+    private List<Reserv> reservList = new ArrayList<>();
+    private List<Client> clientList = new ArrayList<>();        //호텔 손님 목록
     int money;  //보유자산
 
+    public Hotel(int money) {
+        this.money = money;
+    }
 
 
+    public boolean clientUseCheck(String name,String number){       //해당 클라이언트가 현재 호텔 손님 목록에 있는지 체크
+
+
+        for(Client client : clientList){
+            if(client.getName().equals(name)){
+                if(client.getNumber().equals(number)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public Client getOldClient(String name,String number){           // 기존 고객일경우 현재 목록에서 고객반환
+
+
+        for(Client client : clientList){
+            if(client.getName().equals(name)){
+                if(client.getNumber().equals(number)){
+                    System.out.println(client.getName() + " 님은 기존 고객이십니다. \n" );
+                    return client;
+                }
+            }
+        }
+            Client client = new Client("","",0,null);       //이곳에는 코드가 도달하지않음. 예외처리용 코드
+            return client;
+
+
+    }
+
+    public Client getNewClient(String name,String number,int money){        //신규 고객일경우 새롭게 만든후 목록에 추가
+            Client client = new Client(name,number,money,null);
+            clientList.add(client);
+            return client;
+    }
+
+
+
+    public String addReserv(Client client, int reserv_Date , int room_num) {             //고객으로 부터 받은 Reserv를 검증후 reservList에 추가
+        Reserv reserv = new Reserv();
+        String ck = reserv.reserv_Room(client,this.roomList,reserv_Date,room_num);   //검증과정;
+        if (ck.equals("0")) {
+            System.out.println("예악불가 - 잔고부족");
+        }else if(ck.equals("1")){
+            System.out.println("예약불가 - 전화번호 양식 오류");
+
+        }else if(ck.equals("2")){
+            System.out.println("예약불가 - 이미 사용중인 객실");
+        }else{
+            this.reservList.add(reserv);
+            System.out.println("예약성공!");
+            System.out.println("예약번호 : "+ck);
+            //정산
+            client.money-= roomList.getRoom(room_num).getPrice(); // 고객의 돈을 객실 가격만큼 차감
+            this.money = roomList.getRoom(room_num).getPrice(); //호텔의 돈을 객실 가격만큼 증가
+            return ck;
+
+        }
+        return ck;  //이곳에는 도착하지않음.(위의 if문에서 반드시 종료됨)
+
+    }
+
+    public void cancleReserv(UUID id) {             //고객으로 부터 받은 id에 해당하는 reserve를 reservList에서 삭제
+        int cnt = 0;
+        for (Reserv reserv : this.reservList) {
+            if(reserv.client.id.equals(id)) {
+
+                reserv.room.delDate_List(reserv.getDay_date()); // 삭제할 날자 던저줘야함
+                this.reservList.remove(reserv);
+                cnt += 1;
+                break;
+            }
+        }
+        if (cnt == 0) {
+            System.out.println("해당 예약이 존재하지 않습니다.");
+        }
+
+    }
+
+    public void showReservableRoomList(int date) {                  //호텔 해당날짜에 예약가능한 방을 보여주는 기능
+        for (Room room : this.roomList.getArr()) {
+            int cnt = 0;
+            if(room.getDate_list().contains(date)) {
+                cnt +=1;
+            }
+
+            if(cnt == 0) {
+                System.out.println(room);
+            }
+        }
+    }
+
+    public void showClientReserv(UUID id) {                     //고객은 자신의 예약 목록을 조회 할 수 있다.( 예약 번호로 예약 내역을 조회한다)
+        int cnt = 0;
+        for (Reserv reserv : this.reservList) {
+            if(reserv.client.id.equals(id)) {
+                System.out.println(reserv);
+                cnt += 1;
+                break;
+            }
+        }
+        if (cnt == 0) {
+            System.out.println("해당 예약이 존재하지 않습니다.");
+        }
+    }
+    public void showAllReservList() {                  //호텔은 모든 예약 목록을 조회 할 수 있다.
+        for (Reserv reserv : this.reservList) {
+            System.out.println(reserv);
+        }
+    }
+    public void cancelPassed(Date date) {//날짜 받아서 삭제
+        for (Reserv reserv : this.reservList) {
+            if(reserv.dateToDelete.before(date)) {
+                this.reservList.remove(reserv);
+                break;
+            }
+        }
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,181 @@
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.UUID;
+
 public class Main {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        Admin admin = new Admin("1234");
+        Hotel hotel = new Hotel(0);
+        ClientList clientList = new ClientList();
 
-    public static void main(String args[]){
+        String phonePattern = "^\\d{3}-\\d{4}-\\d{4}$";
+        int choice;
+        boolean flag1 = false;
+        boolean flag2 = false;
+        boolean flag3 = false;
+        Reserv reserveList = new Reserv();
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss+'09:00'");
+        System.out.println("현재 날짜입니다.: " + df.format(zonedDateTime));
+
+        while (true) {
+            System.out.println("=====================");
+            System.out.println("ㅐ***Hotel 911***ㅐ");
+            System.out.println("1. 관리자 화면은 숫자 '1'을, 2. 고객이시면 숫자 '2', 3. 프로그램종료는 '3'을 눌러주세요.");
+            System.out.println("=====================");
+            Scanner indexInput = new Scanner(System.in);
+            int num = indexInput.nextInt();
+
+            //관리자 화면
+            switch (num) {
+                case 1:
+                    System.out.println("\n관리자 화면입니다.");
+                    System.out.println("관리자 비밀번호를 입력해주세요");
+
+                    String adminPassword = sc.nextLine();
+                    if (adminPassword.equals(admin.getPassword())) {
+                        while (true) {
+                            System.out.println("\n-----------------------관리자모드-----------------------");
+                            System.out.println("\n1.모든예약내역 확인 2.예약 취소 3.예약가능한객실 확인 4.종료 5. 날짜 스킵");
+                            System.out.println("숫자를 입력해주세요.");
+
+                            choice = sc.nextInt();
+                            sc.nextLine();
+
+                            switch (choice) {
+                                case 1:
+                                    hotel.showAllReservList();
+                                    break;
+                                case 2:
+                                    System.out.println("취소하고자하는 예약 id를 입력해주세요");
+                                    UUID inputId = UUID.fromString(sc.nextLine());
+                                    hotel.cancleReserv(inputId);
+                                    break;
+                                case 3:
+                                    System.out.println("조회하고자하는 날짜를 숫자로 입력해주세요");
+                                    int Inputdate = sc.nextInt();
+                                    sc.nextLine();
+                                    hotel.showReservableRoomList(Inputdate);
+                                    break;
+                                case 4:
+                                    System.out.println("이용 감사합니다.");
+                                    flag2 = true;
+                                    break;
+                                case 5:
+                                    System.out.println("\n넘길 일 수를 입력해주세요.");
+                                    int calDate = sc.nextInt();
+                                    sc.nextLine();
+                                    ZonedDateTime addedDate = zonedDateTime.plusDays(calDate);
+                                    System.out.println("입력한 일 수 만큼 패스한 날짜입니다.: " + df.format(addedDate));
+
+                                    Date date = Date.from(addedDate.toInstant());
+                                    hotel.cancelPassed(date);
+                                    break;
+                                default:
+                                    System.out.println("\n================================");
+                                    System.out.println("****1~5 사이 숫자를 입력해주세요****");
+                                    System.out.println("================================\n");
+                                    break;
+                            }
+                            if (flag2 == true) break;
+                        }
+                    } else {
+                        System.out.println("관리자 비밀번호가 틀렸습니다.");
+                        break;
+                    }
+                    break;
+                case 2:
+                    System.out.println("\n고객 화면입니다.");
+
+                    System.out.println("성함을 입력해주세요.");
+                    String name = sc.nextLine();
+                    System.out.println("전화번호를 입력해주세요.");
+                    String number = sc.nextLine();
+
+                    Client client;  // case 2 에서 사용할 클라이언트 선언
+
+                    if(hotel.clientUseCheck(name,number)){  //호텔이용 목록에있다면
+                        client = hotel.getOldClient(name,number);
+                    }else{      //신규 고객이라면  새로운 고객 생성
+                        System.out.println(name+ " 님은 신규 고객이십니다. \n" );
+                        System.out.println("소지 금액을 입력해주세요\n");
+                        int money = sc.nextInt();
+                        sc.nextLine();
+                        client = hotel.getNewClient(name,number,money);
+                    }
+
+                    System.out.println("\n\n"+name +"님 환영합니다. \n\n" );
 
 
 
-        Hotel hotel = new Hotel();
-        Client client = new Client();
+                    while (true) {
+                        System.out.println("\n-----------------------고객예약-----------------------");
+                        System.out.println("현재 잔액 = " + client.getMoney());
+                        System.out.println("\n1. 예약 하기 2. 예약 조회 3. 예약 취소 4. 예약 가능객실보기 5. 종료");
+                        System.out.println("숫자를 입력해주세요.");
 
 
+                        choice = sc.nextInt();
+                        sc.nextLine();
 
-        hotel.getlist();
-        client.getlist(); //한사람당 여러방 예약가능
+                        switch (choice) {
+                            case 1:
+
+                                System.out.println("예약하고자하는 날짜를 입력해주세요 ex) 2022년11월25일 -> 20221125");
+                                int inputDate = sc.nextInt();
+                                sc.nextLine();
+                                System.out.println("예약하고자하는 객실번호를 입력해주세요 ex) 301호 -> 301");
+                                int inputRoomNum = sc.nextInt();
+                                sc.nextLine();
+                                hotel.addReserv(client, inputDate, inputRoomNum);
+
+                                break;
+                            case 2:
+                                System.out.println("조회하고자하는 예약 id를 입력해주세요");
+                                UUID inputId = UUID.fromString(sc.nextLine());
+                                hotel.showClientReserv(inputId);
+                                break;
+                            case 3:
+                                System.out.println("취소하고자하는 예약 id를 입력해주세요");
+                                UUID inputId2 = UUID.fromString(sc.nextLine());
+                                hotel.cancleReserv(inputId2);
+                                break;
+                            case 4:
+                                System.out.println("조회하고자하는 날짜를 숫자로 입력해주세요");
+                                int Inputdate = sc.nextInt();
+                                sc.nextLine();
+                                hotel.showReservableRoomList(Inputdate);
+                                break;
+                            case 5:
+                                System.out.println("이용 감사합니다.");
+                                flag3 = true;
+                                break;
+                            default:
+                                System.out.println("\n================================");
+                                System.out.println("****1~5 사이 숫자를 입력해주세요****");
+                                System.out.println("================================\n");
+                                break;
+                        }
+                        if (flag3 == true) break;
+                    }
+                    break;
+                case 3:
+                    System.out.println("프로그램을 종료합니다");
+                    flag1 = true;
+                    break;
+                default:
+                    System.out.println("\n================================");
+                    System.out.println("****1~3 사이 숫자를 입력해주세요****");
+                    System.out.println("================================\n");
+                    break;
+            }
+            if (flag1 == true) break;
+        }
+
     }
-
 }

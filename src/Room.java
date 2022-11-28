1fda69415dc7ffdbe9e18e81804cdd1b438b06d9
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2022-11-28 22:23:53 +0900

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -1,3 +1,6 @@
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +11,7 @@ public class Room { //객실
     private int room_num;   //객실호수
     private String type;     //객실타입
 
-    private List<Integer> date_list = new ArrayList<>();   //예약여부 (?)
+    private List<Date> date_list = new ArrayList<>();   //예약여부 (?)
 
     public Room(String type, int room_num) {
         switch (type) {
@@ -46,38 +49,61 @@ public class Room { //객실
         return price;
     }
 
-    public int getRoomNum() {
+    public int getRoom_num() {
         return room_num;
     }
 
-    public List<Integer> getDateList() {
+    public List<Date> getDate_list() {
         return date_list;
     }
 
-    public boolean addDateList(int date) {
+    public boolean addDate_list(String date) {
 
-
-        for(Integer day : date_list){
-            if(day==date){  //중복된 예약일이 있다면
+        Date trans_date = transformDate(date);
+        for(Date day : date_list){
+            if(day==trans_date){  //중복된 예약일이 있다면
                 return false;
             }
-            this.date_list.add(date);
+            this.date_list.add(trans_date);
             return true;        //중복된 예약일없으면 삽입
         }
-        this.date_list.add(date);
+        this.date_list.add(trans_date);
         return true;
     }
 
-    public void delDateList(int x){    //예약일 받음
-        int index = 0;
-        for(Integer day : date_list){
-            if(day==x){  //중복된 예약일이 있다면
-                date_list.remove(index);
-
+    public void delDate_List(String date){    //예약일 받음
+        Date trans_date = transformDate(date);
+        for(Date day : date_list){
+            if(day==trans_date){  //중복된 예약일이 있다면
+                date_list.remove(day);
                 return;
             }
-            index++;
         }
+    }
+
+    public Date transformDate(String date)
+    {
+        SimpleDateFormat beforeFormat = new SimpleDateFormat("yyyy-mm-dd");
+
+        // Date로 변경하기 위해서는 날짜 형식을 yyyy-mm-dd로 변경해야 한다.
+        SimpleDateFormat afterFormat = new SimpleDateFormat("yyyy-mm-dd");
+
+        java.util.Date tempDate = null;
+
+        try {
+            // 현재 yyyymmdd로된 날짜 형식으로 java.util.Date객체를 만든다.
+            tempDate = beforeFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // java.util.Date를 yyyy-mm-dd 형식으로 변경하여 String로 반환한다.
+        String transDate = afterFormat.format(tempDate);
+
+        // 반환된 String 값을 Date로 변경한다.
+        Date d = Date.valueOf(transDate);
+
+        return d;
     }
 
 
